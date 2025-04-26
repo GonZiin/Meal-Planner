5f8dfb6b8554d862c73be946c19d81d0bae5af08
gomes <goncalogomespessoal@outlook.pt>
2025-04-26 15:03:53 +0100

    Procurar Receitas complexas

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def escolherDieta():
     ingredientes = []
     restricao_dieta = []
 
+    pesquisa = input("Receita que queres procurar? ")
     dieta = input("Que tipo de dieta pretendes seguir? ")
 
     escolha_ingredientes = input("Desejas usar algum ingrediente especifio (sim/nao)? ").lower() #Lower para nao ter problema de capitalizacao
@@ -25,7 +26,7 @@ def escolherDieta():
         restricao_dieta.append(input("Que restricao dietetica tens? ").lower()) 
         escolha_restricao = input("Desejas adicionar alguma outra restricao (sim/nao)? ").lower()
 
-    return dieta, ingredientes, restricao_dieta
+    return pesquisa, dieta, ingredientes, restricao_dieta
 
 def receitaPassoAPasso(id):
     url = f"{url_base}/recipes/{id}/analyzedInstructions"
@@ -53,8 +54,26 @@ def receitaComIngredientes(ingredientes, numero):
         if escolha == 'sim':
             receitaPassoAPasso(receita['id'])
 
+def procurarReceita(pesquisa,dieta, ingredientes, restricoes, numero):
+    ingredientes_formatado = ",".join(ingredientes)
+    restricoes_formatado = ",".join(restricoes)
+
+    url = f"{url_base}/recipes/complexSearch"
+    query = {"query":pesquisa, "diet":dieta, "intolerances":restricoes_formatado, "includeIngredients":ingredientes_formatado, "number":numero}
+
+    resposta = requests.get(url, headers=headers, params=query)
+    # Neste caso como retorna um dicionario, as receitas sao o valor da key "results"
+    receitas = resposta.json()["results"]
+    for receita in receitas:
+        print(f"Receita: {receita['title']}")
+        print(f"Id: {receita['id']}")
+        escolha = input("Desejas ver o passo a passo desta receita (sim/nao)? ").lower()
+        if escolha == 'sim':
+            receitaPassoAPasso(receita['id'])
+
 def main():
-    dieta, ingredientes, restricao_dieta = escolherDieta()
-    receitaComIngredientes(ingredientes,1)
+    pesquisa, dieta, ingredientes, restricao_dieta = escolherDieta()
+    #receitaComIngredientes(ingredientes,1)
+    procurarReceita(pesquisa,dieta,ingredientes,restricao_dieta,1)
 
 main()
